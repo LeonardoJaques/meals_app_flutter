0ce48cfe308a4064ea63e6c8a468fb5debccc96e
Leonardo Jaques <jaques.projetos@outlook.com>
2023-05-09 14:32:32 -0300

    add: select category function and material route

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -1,27 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app_flutter/models/category.dart';
 
+import '../screens/categories_meals_screen.dart';
+
 class CategoryItem extends StatelessWidget {
   final Category category;
   const CategoryItem(String title, {super.key, required this.category});
+  void selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) {
+          return const CategoriesMealsScreen();
+        },
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-          gradient: LinearGradient(
-            colors: [
-              category.color.withOpacity(0.5),
-              category.color,
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          )),
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.titleLarge,
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).colorScheme.primary,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            gradient: LinearGradient(
+              colors: [
+                category.color.withOpacity(0.5),
+                category.color,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            )),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
       ),
     );
   }
