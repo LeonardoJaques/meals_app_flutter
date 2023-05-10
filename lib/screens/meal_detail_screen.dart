239c00d665db8876222f1109927ae3dfd6c95956
Leonardo Jaques <jaques.projetos@outlook.com>
2023-05-09 21:18:08 -0300

    add: steps in the meal-detail

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -3,6 +3,34 @@ import 'package:meals_app_flutter/models/meal.dart';
 
 class MealDetailScreen extends StatelessWidget {
   const MealDetailScreen({super.key});
+
+  _createSectionTitle(BuildContext context, String title) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.titleLarge,
+      ),
+    );
+  }
+
+  _createSectionContainer(Widget child) {
+    return Container(
+      width: 300,
+      height: 260,
+      padding: const EdgeInsets.all(10),
+      margin: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(
+          color: Colors.grey,
+        ),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: child,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final meal = ModalRoute.of(context)?.settings.arguments as Meal;
@@ -11,51 +39,55 @@ class MealDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Column(children: [
-        SizedBox(
-          height: 300,
-          width: double.infinity,
-          child: Image.network(
-            meal.imageUrl,
-            fit: BoxFit.cover,
-          ),
-        ),
-        Container(
-          margin: const EdgeInsets.symmetric(vertical: 10),
-          child: Text(
-            'Ingredientes',
-            style: Theme.of(context).textTheme.titleLarge,
-          ),
-        ),
-        Container(
-          width: 300,
-          height: 250,
-          padding: const EdgeInsets.all(10),
-          margin: const EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            border: Border.all(
-              color: Colors.grey,
+      body: SingleChildScrollView(
+        child: Column(children: [
+          SizedBox(
+            height: 200,
+            width: double.infinity,
+            child: Image.network(
+              meal.imageUrl,
+              fit: BoxFit.cover,
             ),
-            borderRadius: BorderRadius.circular(10),
           ),
-          child: ListView.builder(
-            itemCount: meal.ingredients.length,
-            itemBuilder: (ctx, index) {
-              return Card(
-                color: Theme.of(context).colorScheme.secondary,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 5,
-                    horizontal: 10,
+          _createSectionTitle(context, 'Ingredientes'),
+          _createSectionContainer(
+            ListView.builder(
+              itemCount: meal.ingredients.length,
+              itemBuilder: (ctx, index) {
+                return Card(
+                  color: Theme.of(context).colorScheme.secondary,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 10,
+                    ),
+                    child: Text(meal.ingredients[index]),
                   ),
-                  child: Text(meal.ingredients[index]),
-                ),
+                );
+              },
+            ),
+          ),
+          _createSectionTitle(context, 'Passos'),
+          _createSectionContainer(ListView.builder(
+            itemCount: meal.steps.length,
+            itemBuilder: (ctx, index) {
+              return Column(
+                children: [
+                  ListTile(
+                    leading: CircleAvatar(
+                      child: Text('${index + 1}'),
+                    ),
+                    title: Text(meal.steps[index]),
+                  ),
+                  const Divider(
+                    color: Colors.black54,
+                  ),
+                ],
               );
             },
-          ),
-        )
-      ]),
+          ))
+        ]),
+      ),
     );
   }
 }
