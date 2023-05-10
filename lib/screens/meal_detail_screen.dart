7c5bc72ff5f95da9f498631c8847980ef61ff731
Leonardo Jaques <jaques.projetos@outlook.com>
2023-05-10 09:54:00 -0300

    add: select meal implemetation

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -36,58 +36,63 @@ class MealDetailScreen extends StatelessWidget {
     final meal = ModalRoute.of(context)?.settings.arguments as Meal;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(meal.title),
-      ),
-      body: SingleChildScrollView(
-        child: Column(children: [
-          SizedBox(
-            height: 200,
-            width: double.infinity,
-            child: Image.network(
-              meal.imageUrl,
-              fit: BoxFit.cover,
+        appBar: AppBar(
+          title: Text(meal.title),
+        ),
+        body: SingleChildScrollView(
+          child: Column(children: [
+            SizedBox(
+              height: 200,
+              width: double.infinity,
+              child: Image.network(
+                meal.imageUrl,
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          _createSectionTitle(context, 'Ingredientes'),
-          _createSectionContainer(
-            ListView.builder(
-              itemCount: meal.ingredients.length,
-              itemBuilder: (ctx, index) {
-                return Card(
-                  color: Theme.of(context).colorScheme.secondary,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 5,
-                      horizontal: 10,
+            _createSectionTitle(context, 'Ingredientes'),
+            _createSectionContainer(
+              ListView.builder(
+                itemCount: meal.ingredients.length,
+                itemBuilder: (ctx, index) {
+                  return Card(
+                    color: Theme.of(context).colorScheme.secondary,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 5,
+                        horizontal: 10,
+                      ),
+                      child: Text(meal.ingredients[index]),
                     ),
-                    child: Text(meal.ingredients[index]),
-                  ),
+                  );
+                },
+              ),
+            ),
+            _createSectionTitle(context, 'Passos'),
+            _createSectionContainer(ListView.builder(
+              itemCount: meal.steps.length,
+              itemBuilder: (ctx, index) {
+                return Column(
+                  children: [
+                    ListTile(
+                      leading: CircleAvatar(
+                        child: Text('${index + 1}'),
+                      ),
+                      title: Text(meal.steps[index]),
+                    ),
+                    const Divider(
+                      color: Colors.black54,
+                    ),
+                  ],
                 );
               },
-            ),
-          ),
-          _createSectionTitle(context, 'Passos'),
-          _createSectionContainer(ListView.builder(
-            itemCount: meal.steps.length,
-            itemBuilder: (ctx, index) {
-              return Column(
-                children: [
-                  ListTile(
-                    leading: CircleAvatar(
-                      child: Text('${index + 1}'),
-                    ),
-                    title: Text(meal.steps[index]),
-                  ),
-                  const Divider(
-                    color: Colors.black54,
-                  ),
-                ],
-              );
-            },
-          ))
-        ]),
-      ),
-    );
+            ))
+          ]),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.star),
+          onPressed: () {
+            Navigator.of(context).pop(meal.title);
+          },
+        ));
   }
 }
